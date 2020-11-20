0b92380235b87db2eb5f5b7c5c323dc9a530345f
pratyadokbua <73688737+pratyadokbua@users.noreply.github.com>
2020-11-20 11:45:12 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'prattya.mysql.database.azure.com', 'it63070100', 'YBPfrp64', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$id = $_GET['id'];
+$sql = "DELETE FROM guestbook WHERE id = $id";
+if (mysqli_query($conn, $sql)) {
+echo "Record deleted successfully";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
