e0f9916ea8e8bee9eca0a491d8abd19392dba1ad
pratyadokbua <73688737+pratyadokbua@users.noreply.github.com>
2020-11-20 11:44:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -26,6 +27,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
+    <td><?php echo '<a href="delete.php?id='.$Result['id'].'">Delete</a>'?></td>
   </tr>
 <?php
 }
