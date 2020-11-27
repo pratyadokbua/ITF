3d41837cde9807f221d48bcf66ef1c126565ee11
pratyadokbua <73688737+pratyadokbua@users.noreply.github.com>
2020-11-27 15:04:36 +0700

    Add files via upload

---
+++ Delete.php
@@ -0,0 +1,21 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'prattya.mysql.database.azure.com', 'it63070100', 'YBPfrp64', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$id = $_GET['ID'];
+$sql = "DELETE FROM totalprice WHERE ID = $ID";
+if (mysqli_query($conn, $sql)) {
+echo "Record deleted successfully";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
+<html>
+<body>
+  <a href="https://sometable.azurewebsites.net/">Back_to_Base</a>
+  </body>
+</html>
