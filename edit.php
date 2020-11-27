d96ae6d3c7ee0f06f21d239a1f8d5bde7badc12e
pratyadokbua <73688737+pratyadokbua@users.noreply.github.com>
2020-11-27 15:44:15 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -6,7 +6,7 @@ if (mysqli_connect_errno($conn))
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $id = $_GET['ID'];
-$sql = "UPDATE FROM totalprice (Price=$Price, Discount=%Discount), WHERE id =$ID";
+$sql = "UPDATE FROM totalprice Value (Price=$Price, Discount=%Discount), WHERE id =$ID";
 if (mysqli_query($conn, $sql)) {
 echo "Record Update successfully";
 } else {
