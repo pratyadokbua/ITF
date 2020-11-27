7bfaccf14242a55ab868c328cf5c84a7220149e5
pratyadokbua <73688737+pratyadokbua@users.noreply.github.com>
2020-11-27 15:26:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Price'];?></td>
     <td><?php echo $Result['Discount'];?></td>
     <td><?php echo $Result['Discount'];?></td>
-    <td><?php echo '<a href="delete.php?id='.$Result['id'].'">Delete</a>'?></td>
+    <td><?php echo '<a href="edit.php?id='.$Result['id'].'">Edit</a>'?></td>
   </tr>
 <?php
 }
